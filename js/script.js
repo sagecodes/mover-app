6c048273a4c5f8bc1d9ffe72461c126e51cd3ab7
Sage Elliott <wildtroll@gmail.com>
2016-01-21 16:23:18 -0800

    Feat: Load streetview background image
    - take params from form and convert to address for streetview api
    - change greeting text on load of address
    - append image from streeview api to background

--- js/script.js
+++ js/script.js
@@ -12,8 +12,15 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     return false;
 };
